d4142257f42b43ea1bbc80079d76d18a9ed0aaf6
Joseph <osekuemen@gmail.com>
2021-08-19 12:42:59 +0100

    step 1 - internet connection with ScalarsConverter and Retrofit

--- app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -6,15 +6,6 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("imageUrl")
-fun bindImage(imgView: ImageView, imgUrl: String?) {
-    imgUrl?.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-        Glide.with(imgView.context)
-            .load(imgUri)
-            .into(imgView)
-    }
-}
 
 @BindingAdapter("statusIcon")
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,57 +1,43 @@
 package com.udacity.asteroidradar.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.udacity.asteroidradar.Asteroid
-import com.udacity.asteroidradar.PictureOfDay
-import com.udacity.asteroidradar.api.AsteroidApi
-import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import com.udacity.asteroidradar.api.NasaApi
+import retrofit2.Response
 
 
 class MainViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the most recent response
-    private val _asteroids = MutableLiveData<List<Asteroid>>()
-    // The external immutable LiveData for the response String
-    val asteroids: LiveData<List<Asteroid>>
-        get() = _asteroids
-
-    private val _imageOfDay = MutableLiveData<PictureOfDay>()
-    val imageOfDay: LiveData<PictureOfDay>
-        get() = _imageOfDay
-
     private val _response = MutableLiveData<String>()
     val response :LiveData<String>
     get() = _response
+
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getAsteroids()
-        getTodaysImage()
-    }
-
-    /**
-     * Sets the value of the response LiveData to the Asteroid API status or the successful number of
-     * Asteroid properties retrieved.
-     */
-    private fun getAsteroids() {
+        getResponse()
 
     }
 
-    private fun getTodaysImage() {
-        viewModelScope.launch {
-            try {
-                var listResult = AsteroidApi.retrofitService.getPictureOfDay()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
-                if (listResult.size > 0) {
-                    _imageOfDay.value = (listResult[0])
-                }
-            } catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+    private fun getResponse()
+    {
+        NasaApi.retrofitService.getProperties().enqueue(object:Callback<String>{
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
             }
-        }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure" + t.message
+            }
+
+        })
+        _response.value = "Set the api response here."
     }
+
+
 }

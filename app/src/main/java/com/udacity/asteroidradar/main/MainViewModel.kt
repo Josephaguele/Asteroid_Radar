c3cb07bd8f7e6c1b3e1e5404aeb2d2fdaab0ccda
Joseph <osekuemen@gmail.com>
2021-08-19 15:25:29 +0100

    step 1 - At this stage, the picture of the day has been generated in String format, however, the asteroid cannot be generated with Moshi so a method called parseJsonResult has to be used. A textview was also added to crosscheck the data generation for the Asteroid data

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.PictureOfDay
 import retrofit2.Call
 import retrofit2.Callback
 import com.udacity.asteroidradar.api.NasaApi
@@ -16,27 +18,46 @@ class MainViewModel : ViewModel() {
     val response :LiveData<String>
     get() = _response
 
+    private val _asResponse = MutableLiveData<String>()
+    val asResponse : LiveData<String>
+    get() = _asResponse
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
         getResponse()
+        getAsteroidResponse()
 
     }
 
     private fun getResponse()
     {
-        NasaApi.retrofitService.getProperties().enqueue(object:Callback<String>{
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body()
+        NasaApi.retrofitService.getProperties().enqueue(object:Callback<PictureOfDay>{
+            override fun onResponse(call: Call<PictureOfDay>, response: Response<PictureOfDay>) {
+                _response.value = response.body().toString()
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<PictureOfDay>, t: Throwable) {
                 _response.value = "Failure" + t.message
             }
 
         })
         _response.value = "Set the api response here."
+    }
+
+    private fun getAsteroidResponse()
+    {
+        NasaApi.retrofitService.getAsteroidList().enqueue(object:Callback<Asteroid>{
+            override fun onResponse(call: Call<Asteroid>, response: Response<Asteroid>) {
+                _asResponse.value =  response.message()
+            }
+
+            override fun onFailure(call: Call<Asteroid>, t: Throwable) {
+                _asResponse.value = " Failure" + t.message
+            }
+
+        })
+        _asResponse.value = "Set the Asteroid Response here."
     }
 
 

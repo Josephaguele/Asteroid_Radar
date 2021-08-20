f7a5f33e8feffcd67b2b546ae03dab4e833c44f8
Joseph <osekuemen@gmail.com>
2021-08-20 16:02:13 +0100

    step 1 - Asteroids loading failing

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,13 +1,14 @@
 package com.udacity.asteroidradar.main
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.NasaApi
+import com.udacity.asteroidradar.api.NasaAsteroidsApi
 import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
@@ -52,13 +53,13 @@ class MainViewModel : ViewModel() {
     
     private fun getAsteroids()
     {
-        NasaApi.retrofitService.getAsteroidList().enqueue(object : Callback<String>{
+        NasaAsteroidsApi.retrofitService.getAsteroidList().enqueue(object : Callback<String>{
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                _asteroids.value = response.body()?.
-                let { parseAsteroidsJsonResult(JSONObject(it)) }
+                    _asteroids.value = response.body()?.let { parseAsteroidsJsonResult(JSONObject(it)) }
             }
             override fun onFailure(call: Call<String>, t: Throwable) {
-                _response.value = t.message
+                _response.value = "Failure:"+t.message
+
             }
         })
     }
